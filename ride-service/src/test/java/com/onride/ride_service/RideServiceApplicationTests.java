fbeb82c9e48752b0e4656ebdece11e51d4a55bf3
Aditya Mihra <aditya.mishra@devrev.ai>
2026-08-23 16:58:32 +0530

    feat(ride): scaffolded ride service template

---
+++ ride-service/src/test/java/com/onride/ride_service/RideServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onride.ride_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
